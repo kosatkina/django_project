0b3f2006151229798fad9e30fcaf4d7457317061
kosatkina <tatiana.kosatkina@gmail.com>
2025-03-21 10:20:04 -0400

    Added spinner to the main page to show status of loading posts

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -21,10 +22,13 @@ $.ajax({
     success: function(response) {
         console.log('success:', response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
-        });
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
+            });
+        }, 100)
     },
     error: function(error) {
         console.log('error:', error)
