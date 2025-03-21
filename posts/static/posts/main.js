1cc93acec0d373cf93757898b6d62e01b2daded8
kosatkina <tatiana.kosatkina@gmail.com>
2025-03-21 10:08:22 -0400

    Created AJAX call to get the post object and display on the web page

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,15 +1,32 @@
 console.log('hello')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 $.ajax({
     type: 'GET',
     url: '/hello-world/',
     success: function(response) {
-        console.log(response.text)
+        console.log('success: ', response.text)
         helloWorldBox.textContent = response.text
     },
     error: function(error) {
-        console.log('error', error)
+        console.log('error:', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log('success:', response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `${el.title} - <b>${el.body}</b><br>`
+        });
+    },
+    error: function(error) {
+        console.log('error:', error)
     }
 })
