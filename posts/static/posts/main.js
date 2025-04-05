c1a3fe2984a8c773c4750179b76f98c15e21d015
kosatkina <tatiana.kosatkina@gmail.com>
2025-04-05 12:40:27 -0400

    Added a function to render post details and display it a new page

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -11,6 +11,7 @@ const title = document.getElementById('id_title')
 const body = document.getElementById('id_body')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
+const url = window.location.href
 const alertBox = document.getElementById('alert-box')
 
 console.log('csrf: ', csrf[0].value)
@@ -80,7 +81,7 @@ const getData = () => {
                             <div class="card-footer">
                                 <div class="row">
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">Details</a>
+                                        <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
@@ -155,7 +156,7 @@ postForm.addEventListener('submit', e => {
                 // Hide modal authomatically and notify that new post was created
                 $('#addPostModal').modal('hide')
                 handleAlerts('success', 'New post added!')
-
+                postForm.reset()
         },
         error: function(error) {
             console.log(error)
